b87d5d1aabfed18f014e6ef00e7b1a72e26471dc
Rose Sawyer <rosesawyer@Roses-MacBook-Air.local>
2020-03-30 18:12:54 -0400

    added mutations for author and book

---
+++ server/models/book.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const bookSchema = new Schema({
+    title: String,
+    era: String,
+    authorId: String
+})
+
+module.exports = mongoose.model('Book', bookSchema)
+
+//model refers to a collection in a database(mlab)
