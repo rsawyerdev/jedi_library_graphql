2ee4e54326a51a33bcbe413b74da949c4e9db57f
Rose Sawyer <rosesawyer@Roses-MacBook-Air.local>
2020-04-07 22:32:50 -0400

    react front end added

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const styles = {
+  container: {
+    width: '100%',
+    height: 1000,
+    backgroundColor: 'lightgrey'
+  }
+}
+
+function App() {
+  return (
+    <div style={styles.container}>
+      <h1>Jedi Reading List</h1>
+      
+    </div>
+  );
+}
+
+export default App;
